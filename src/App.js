98c5d92c1f6e3c95bff8c29076dbfc0d71500a4e
Alon Ben Haim <alon.b.h.672@gmail.com>
2020-12-08 20:30:11 +0200

    wip

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import React, { Component } from 'react';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
+// import Importer from './components/Importer/Importer';
+// import Select from './components/Importer/Select/Select';
+
 class App extends Component {
   render() {
     return (
       <Layout>
         <BurgerBuilder />
       </Layout>
+        // <Importer />
+        // <Select />
     );
   }
 }
